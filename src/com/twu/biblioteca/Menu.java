b5514b3dc1a84af6458ea8f0de6bc5b530711e6e
Xinqian liu <crystalynliu@163.com>
2016-08-08 13:49:10 +0800

    Refactor menu class and initial menu list

--- src/com/twu/biblioteca/Menu.java
+++ src/com/twu/biblioteca/Menu.java
@@ -1,11 +1,44 @@
 package com.twu.biblioteca;
 
 
-public class Menu {
-    private String menuFirst = "1. LIST BOOKS";
-    private String menuSecond = "2. CHOCK OUT BOOK";
-    private String menuThird = "3. RETURN BOOK";
-    private String menuFourth = "4. QUIT";
+import java.util.ArrayList;
+import java.util.List;
 
-    private String invalidMenuMessage = "Select a valid option!";
+public class Menu {
+    private int menuId;
+    private String menuName;
+
+    private static String invalidMenuMessage = "Select a valid option!";
+
+    public Menu(int menuId,String menuName){
+        this.menuId = menuId;
+        this.menuName = menuName;
+    }
+
+    public void setMenuId(int menuId){
+        this.menuId = menuId;
+    }
+
+    public int getMenuId(){
+        return this.menuId;
+    }
+
+    public void setMenuName(String menuName){
+        this.menuName = menuName;
+    }
+
+    public String getMenuName(){
+        return this.menuName;
+    }
+
+    public static List<Menu> InitialMenuList(){
+        List<Menu> menuList = new ArrayList<Menu>();
+        menuList.add(new Menu(1, "LIST BOOKS"));
+        menuList.add(new Menu(2, "CHOCK OUT BOOK"));
+        menuList.add(new Menu(3, "RETURN BOOK"));
+        menuList.add(new Menu(4, "QUIT"));
+
+        return menuList;
+    }
+
 }
