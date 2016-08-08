ccdb42e679f27e4720ce59bc6056ff428b2a3ee3
Xinqian liu <crystalynliu@163.com>
2016-08-08 13:26:58 +0800

    complete printBookList pass the test

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -1,5 +1,6 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Book {
@@ -45,6 +46,24 @@ public class Book {
         return this.yearPublished;
     }
 
-    public void printBookList() {
+    public static List<Book> initialBookList(){
+        List<Book> bookList = new ArrayList<Book>();
+        bookList.add(new Book(1,"Harry Potter and the Sorcerer's Stone","Rowling","1997"));
+        bookList.add(new Book(2,"Charlie and the Chocolate Factory","Dahl","1964"));
+        bookList.add(new Book(3,"The Great Gatsby ","Fitzgerald","1925"));
+        bookList.add(new Book(4,"The Old Man and the Sea","Hemingway","1952"));
+        bookList.add(new Book(5,"The Catcher in the Rye","Salinger","1951"));
+
+        return bookList;
+    }
+
+    public void printBookList(List<Book> bookList) {
+        for(int i = 0; i < bookList.size(); i++){
+            Book currentBook = bookList.get(i);
+            System.out.println(currentBook.bookId + " | "
+                    + "name:" + currentBook.bookName + " | "
+                    + "author:" + currentBook.author + " | "
+                    + "published:" + currentBook.yearPublished);
+        }
     }
 }
